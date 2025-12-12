262d91929fc312e7d4c54807680ca2f63b316096
shubhamraj82 <raj.shubh8216@gmail.com>
2025-12-12 22:55:23 +0530

    scrolltriger with video of drink added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { SplitText } from 'gsap/SplitText'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'   
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Cocktails from './components/Cocktails';
 
-gsap.registerPlugin(ScrollTrigger, SplitText)   
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]' >
-        <h1 className='text-3xl text-indigo-300 text-gradient'>Hello Gsap</h1>
-    </div>
+   <main>
+    <Navbar/>
+    <Hero/>
+    <Cocktails/>
+    
+   </main>
   )
 }
 
---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Cocktails = () => {
+  return (
+   <section id='cocktails' className='noisy'>
+    <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+	 <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+
+     <div className='list'>
+        <div className='popular'>
+            <h2></h2>
+        </div>
+     </div>
+   </section>
+  )
+}
+
+export default Cocktails
