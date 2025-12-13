763ee3608df7b3118dfe3c93e88eb3134e33f678
shubhamraj82 <raj.shubh8216@gmail.com>
2025-12-13 14:35:12 +0530

    art animation done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const App = () => {
     <Hero/>
     <Cocktails/>
     <About/>
+    <Art/>
     
    </main>
   )
